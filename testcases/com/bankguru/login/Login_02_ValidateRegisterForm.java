ea609b626a56fdb50969ed66d78af419cd3e7ed3
Phan Thi Kim Hoa <hoakimphan1996@gmail.com>
2019-02-12 22:15:44 +0700

    Add 01

---
+++ testcases/com/bankguru/login/Login_02_ValidateRegisterForm.java
@@ -0,0 +1,31 @@
+package com.bankguru.login;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Login_02_ValidateRegisterForm {
+  @Test
+  public void TC_01_LoginWithEmailInvalid() {
+  }
+  @Test
+  public void TC_02_LoginWithEmailIncorrect() {
+  }
+  @Test
+  public void TC_03_LoginWithPasswordLessThan6Chars() {
+  }
+  @Test
+  public void TC_04_LoginWithPasswordIncorrect() {
+  }
+  @Test
+  public void TC_05_LoginWithUserAndPasswordCorrect() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
